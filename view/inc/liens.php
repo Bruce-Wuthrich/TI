5b68c63eb2d7e651ca4eb4a93ca2664085a2729c
Bruce-Wuthrich <azertytagada8@gmail.com>
2026-04-07 10:54:57 +0200

    rajout des ul li des hambergiline

---
+++ view/inc/liens.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Liens</title>
+    <link rel="stylesheet" href="/public/css/style.css">
+</head>
+<body>
+     <nav>
+            <div class="navbarre">
+                <div id="logo">
+                    <img src="/public/img/banniere.jpg">
+                </div>
+                <input type="checkbox" id="nav-toggle" class="nav-toggle">
+                <label for="nav-toggle" class="nav-toggle-label" aria-label="Menu">
+                    <span class="hamburgerline"></span>
+                    <span class="hamburgerline"></span>
+                    <span class="hamburgerline"></span>
+                </label>
+                <ul id="liens">
+                <li><a href="acceuil.php">Accueil</a></li>
+                <li><a href="geographie.php">Geographie</a></li>
+                <li><a href="histoire.php">Histoire</a></li>
+                <li><a href="culture.php">Culture</a></li>
+                <li><a href="galerie.php">galerie</a></li>
+                <li><a href="contact.php">Contacts</a></li>
+                <li><a href="liens.php">Liens</a></li>
+                </ul>
+            </div>
+    </nav>
+    
+</body>
+</html>
