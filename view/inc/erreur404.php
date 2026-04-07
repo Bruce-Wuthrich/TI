b4259b5a88b1af84d051621a7482656410e2dc79
Bruce-Wuthrich <azertytagada8@gmail.com>
2026-04-07 13:14:43 +0200

    rajout du css

--- view/inc/erreur404.php
+++ view/inc/erreur404.php
@@ -2,14 +2,14 @@
 if(!isset($_GET['p'])){
 
 
- include ROOT_PATH."./view/inc/menu.php";
+ include ROOT_PATH."/view/inc/menu.php";
 
 }elseif(in_array($_GET['p'],ARRAY_VALID_PAGES)){
 
-    include ROOT_PATH."./view/inc/".$_GET['p'].".php";
+    include ROOT_PATH."/view/inc/".$_GET['p'].".php";
     
 }else{
-    include ROOT_PATH."./view/inc/erreur404.php";
+    include ROOT_PATH."/view/inc/erreur404.php";
 }
 ?>
 <!DOCTYPE html>
--- view/inc/menu.php
+++ view/inc/menu.php
@@ -3,20 +3,14 @@
 
 # ...
 
-require_once '../config.php';
+require_once './config.php';
 
-/*******************
- * Routage entre les
- * diverses vues
- *******************/
 
-// si non existence de la variable
-// $_GET nommée 'p'
 if(!isset($_GET['p'])){
 
     // Nous sommes sur l'accueil
     // chargement de view/homepage.php
-    include ROOT_PATH."../view/inc/menu.php";
+    include ROOT_PATH."./view/inc/menu.php";
 
 }
 ?>
