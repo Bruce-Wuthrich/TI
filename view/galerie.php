47ad456c0e266f7ad86c1070f7c12f4202c0441a
Bruce-Wuthrich <azertytagada8@gmail.com>
2026-04-08 09:58:25 +0200

    finition de tout les pages php en html structuration

---
+++ view/galerie.php
@@ -0,0 +1,46 @@
+<div class="contenu-page">
+
+    <h2 class="titre-page">Galerie photographique</h2>
+
+    <div class="galerie-grille">
+
+        <div class="galerie-image">
+                <img src="img/480px_Acropole_Caryatides.jpg">
+            </a>
+            <p>Les Caryatides de l'Acropole</p>
+        </div>
+
+        <div class="galerie-image">
+                <img src="img/480px_Attica_Athens_View_from_Acropolis_Hill.jpg">
+            </a>
+            <p>Vue d'Athènes depuis la colline de l'Acropole</p>
+        </div>
+
+        <div class="galerie-image">
+                <img src="img/480px_monument.jpg">
+            </a>
+            <p>Monument emblématique d'Athènes</p>
+        </div>
+
+        <div class="galerie-image">
+
+                <img src="img/480px_Odeon_of_Herodes_Atticus.jpg">
+            </a>
+            <p>L'Odéon d'Hérode Atticus</p>
+        </div>
+
+        <div class="galerie-image">
+                <img src="img/480px_Temple_of_Zeus_in_Athens.jpg">
+            </a>
+            <p>Le Temple de Zeus Olympien</p>
+        </div>
+
+        <div class="galerie-image">
+                <img src="img/480px_View_of_the_Acropolis_Athens.jpg">
+            </a>
+            <p>Vue de l'Acropole d'Athènes</p>
+        </div>
+
+    </div>
+
+</div>
--- view/liens.php
+++ view/liens.php
@@ -1,2 +1,33 @@
 
-  
+  <div class="contenu-page">
+
+    <h2 class="titre-page">Liens pour rejoindre les autres capitales</h2>
+
+    <div class="img-centree">
+        <img src="img/liens.jpg" alt="Capitales européennes">
+    </div>
+    <div class="center">
+    <p>Découvrez les sites dédiés aux autres capitales européennes :</p>
+    </div>
+    <ul class="liens-liste">
+        <li><a href="">Berlin</a></li>
+        <li><a href="">Vienne</a></li>
+        <li><a href="">Paris</a></li>
+        <li><a href="">Madrid</a></li>
+        <li><a href="">Rome</a></li>
+        <li><a href="">Londres</a></li>
+        <li><a href="">Lisbonne</a></li>
+        <li><a href="">Prague</a></li>
+        <li><a href="">Amsterdam</a></li>
+        <li><a href="">Copenhague</a></li>
+        <li><a href="">Dublin</a></li>
+        <li><a href="">Athènes</a></li>
+        <li><a href="">Sofia</a></li>
+        <li><a href="">Varsovie</a></li>
+        <li><a href="">Helsinki</a></li>
+        <li><a href="">Stockholm</a></li>
+        <li><a href="">Oslo</a></li>
+        <li><a href="">Budapest</a></li>
+    </ul>
+
+</div>
